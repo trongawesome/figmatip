efaeff1c2177c4e8943f162badf8c929a8d6a781
Trong Nguyen <tantrongtt@gmail.com>
2020-02-25 14:49:01 +0800

    add background

--- @narative/gatsby-theme-novela/src/sections/articles/Articles.Hero.tsx
+++ @narative/gatsby-theme-novela/src/sections/articles/Articles.Hero.tsx
@@ -3,13 +3,14 @@ import { graphql, useStaticQuery } from 'gatsby';
 import styled from '@emotion/styled';
 import { Link } from 'gatsby';
 
+import Image from '@components/Image';
 import Section from '@components/Section';
 import mediaqueries from '@styles/media';
 import { IAuthor } from '@types';
 
 import { GridLayoutContext } from './Articles.List.Context';
 
-const authorQuery = graphql`
+const myQuery = graphql`
   {
     site: allSite {
       edges {
@@ -23,43 +24,78 @@ const authorQuery = graphql`
         }
       }
     }
+
+    file(relativePath: {eq: "site-hero.jpg"}) {
+      id
+      childImageSharp {
+        fluid(maxWidth: 1200, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+
   }
 `;
+
 
 const ArticlesHero: React.FC<IAuthor> = ({ authors }) => {
   const { gridLayout = 'tiles', hasSetGridLayout, setGridLayout } = useContext(
     GridLayoutContext,
   );
 
-  const results = useStaticQuery(authorQuery);
+  const results = useStaticQuery(myQuery);
   const hero = results.site.edges[0].node.siteMetadata.hero;
+  const heroImage = results.file.childImageSharp.fluid;
   
-  // Hide author
-  // const featuredAuthor = authors.find(author => author.featured);
-
-  // if (!featuredAuthor) {
-  //   throw new Error(`
-  //     No featured Author found.Trong test ssh.
-  //     Please ensure you have at least featured Author.
-  // `);
-  // }
-
   return (
-    <Section narrow id="Articles__Hero">
-      <HeadingContainer style={{ maxWidth: `${hero.maxWidth}px` }}>
-        <HeroHeading dangerouslySetInnerHTML={{ __html: hero.writingHeading }} />
-        <InfoText>
-          Without words, apps would be an unusable jumble of shapes and icons.
-        </InfoText>
-      </HeadingContainer>
-    </Section>
+    <HeroContainer>
+      <TextWrap>
+        <Section narrow id="Articles__Hero">
+          <HeadingContainer style={{ maxWidth: `${hero.maxWidth}px` }}>
+            <HeroHeading dangerouslySetInnerHTML={{ __html: hero.writingHeading }} />
+            <InfoText>
+              Without words, apps would be an unusable jumble of shapes and icons.
+            </InfoText>
+          </HeadingContainer>
+        </Section>
+      </TextWrap>
+      <ImageWrap>
+        <Image
+          src={heroImage}
+          // imgStyle={{ objectFit: 'contain', objectPosition: 'bottom' }}
+        />
+      </ImageWrap>
+    </HeroContainer>
   );
 };
 
 export default ArticlesHero;
 
+const HeroContainer = styled.div`
+  position: relative;
+  display: block;
+  overflow: hidden;
+  margin-top: -100px;
+  height: 840px;
+`;
+
+const ImageWrap = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block
+  height: 100%;
+  width: 100%;
+`;
+
+const TextWrap = styled.div`
+  position: relative;
+  z-index: 1;
+`;
+
+
 const HeadingContainer = styled.div`
-  margin: 104px 0 40px;
+  margin: 250px 0 40px;
   
   ${mediaqueries.desktop`
   width: 80%;
@@ -70,7 +106,7 @@ const HeadingContainer = styled.div`
   `}
   `;
   
-  const HeroHeading = styled.h1`
+const HeroHeading = styled.h1`
   font-style: normal;
   font-weight: ${p => p.theme.fontsWeight.bold};
   font-size: 64px;
