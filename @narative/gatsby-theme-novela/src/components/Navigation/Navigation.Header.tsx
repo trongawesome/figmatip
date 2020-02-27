2fd2149c801995c4c0a08f061e744b38e7e0fecd
Trong Nguyen <tantrongtt@gmail.com>
2020-02-27 15:58:39 +0800

    adjust navigation

--- @narative/gatsby-theme-novela/src/components/Navigation/Navigation.Header.tsx
+++ @narative/gatsby-theme-novela/src/components/Navigation/Navigation.Header.tsx
@@ -137,8 +137,8 @@ const NavContainer = styled.div`
 
 const NavLink = styled(Link)`
   font-weight: ${p => p.theme.fontsWeight.bold};
-  font-family: ${p => p.theme.fonts.title};
-  font-size: 12px;
+  font-family: ${p => p.theme.fonts.body};
+  font-size: 14px;
   color: ${p => p.theme.colors.secondary};
   transition: color 0.25s var(--ease-in-out-quad);
   display: inline-block;
