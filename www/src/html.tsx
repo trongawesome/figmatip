40d38fb56cda063e0bbf3dca6572b5f94a2c003b
Trong <tantrongtt@gmail.com>
2022-03-06 20:52:10 +0700

    update omain

--- www/src/html.tsx
+++ www/src/html.tsx
@@ -11,7 +11,7 @@ export default props => (
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
       />
       <script
-        dangerouslySetInnerHTML={{ __html: `document.domain = "awesomefigmatips.com"` }}
+        dangerouslySetInnerHTML={{ __html: `document.domain = "awesomefigtips.com"` }}
       />
       {props.headComponents}
     </head>
